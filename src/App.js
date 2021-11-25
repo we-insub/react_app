2a85aeff45f814c1904d50428396ca3b84f7cbe8
weinsub <dnlstjq@naver.com>
2021-11-25 11:12:04 +0900

    2021.11.25 2021.11.25 cleanup -            Todo List _ map() 사용하기, 간단한 todoList            coinPriceTag _ API 호출해서 useEffect 사용하여,                           처음, 계속 불러오는것 설정하고 불러오기 2021.11.24 Make a react_app            make a button            useEffect() _ 호출이 계속되야하는것과 한번만 호출해도되는것                        즉, 클릭값은 계속확인하면서 api 호출은 한번만            Deps _ 함수내에서 원하는 것만 호출하기 (ex)                   클릭카운트 , 키입력값 출력 두개동시가아닌 원하는것만

--- src/App.js
+++ src/App.js
@@ -3,40 +3,33 @@ import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]); // ...append 엘리멘트리에 추가하기.
-    // 즉 입력값 todo가 커렌트어레이자리로오고 2번째입력한값이 다시 투두 커렌트에는
-    // 처음입력한값 세번째값을 입력할경우 1,2 번째입력한값이 커렌트어레이에들어감.
-    setToDo("");
-  };
-  console.log(toDos); // 값 체크하기위해서 작성코드
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-      {toDos.map((item, index) => (
-        <li key={index}>{item}</li>
-      ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
+
 
 export default App;
