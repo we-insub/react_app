8659fcee833f85af98be6a8d6cf0e4e74980c1e6
weinsub <dnlstjq@naver.com>
2021-11-25 10:32:29 +0900

    2021.11.25 2021.11.25 cleanup -            Todo List _ map() 사용하기, 간단한 todoList 2021.11.24 Make a react_app            make a button            useEffect() _ 호출이 계속되야하는것과 한번만 호출해도되는것                        즉, 클릭값은 계속확인하면서 api 호출은 한번만            Deps _ 함수내에서 원하는 것만 호출하기 (ex)                   클릭카운트 , 키입력값 출력 두개동시가아닌 원하는것만

--- src/App.js
+++ src/App.js
@@ -1,45 +1,40 @@
 import Button from "./Button"
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
-function Hello() {
-  useEffect(function () {
-    console.log("hi :)");
-    return function () {
-      console.log("bye :(");
-    };
-  }, []);
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-  return <h1>Hello</h1>;
-}
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setShowing((prev) => !prev);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => {
-    console.log("I run only once.");
-  }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]); 
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when keyword & counter change");
-  }, [keyword, counter]);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]); // ...append 엘리멘트리에 추가하기.
+    // 즉 입력값 todo가 커렌트어레이자리로오고 2번째입력한값이 다시 투두 커렌트에는
+    // 처음입력한값 세번째값을 입력할경우 1,2 번째입력한값이 커렌트어레이에들어감.
+    setToDo("");
+  };
+  console.log(toDos); // 값 체크하기위해서 작성코드
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here...." />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+      {toDos.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+      </ul>
     </div>
   );
 }
