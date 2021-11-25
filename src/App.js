859adda86be5de865863204a3d36c4bad5fcf679
weinsub <dnlstjq@naver.com>
2021-11-25 17:34:01 +0900

    2021.11.25 2021.11.25 cleanup -            Todo List _ map() 사용하기, 간단한 todoList            coinPriceTag _ API 호출해서 useEffect 사용하여,                           처음, 계속 불러오는것 설정하고 불러오기            Detail.js -> 수정중임, 홈에서 status 사용한것을 사용해서                         Json을 이용해서 완성하기 숙제중임. 2021.11.24 Make a react_app            make a button            useEffect() _ 호출이 계속되야하는것과 한번만 호출해도되는것                        즉, 클릭값은 계속확인하면서 api 호출은 한번만            Deps _ 함수내에서 원하는 것만 호출하기 (ex)                   클릭카운트 , 키입력값 출력 두개동시가아닌 원하는것만

--- src/App.js
+++ src/App.js
@@ -1,35 +1,22 @@
-import Button from "./Button"
-import styles from "./App.module.css";
-import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
   return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option>
-              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-      )}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/abot-us">
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
-
-
 export default App;
