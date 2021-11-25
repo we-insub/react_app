0a9f1745d7272bf1b005ffc7e2aadb4130d6a0ff
weinsub <dnlstjq@naver.com>
2021-11-25 09:32:26 +0900

    2021.11.25 2021.11.24 Make a react_app            make a button            useEffect() _ 호출이 계속되야하는것과 한번만 호출해도되는것                        즉, 클릭값은 계속확인하면서 api 호출은 한번만            Deps _ 함수내에서 원하는 것만 호출하기 (ex)                   클릭카운트 , 키입력값 출력 두개동시가아닌 원하는것만 2021.11.25 cleanup -
    2021.11.24 Make a react_app
               make a button
               useEffect() _ 호출이 계속되야하는것과 한번만 호출해도되는것
                           즉, 클릭값은 계속확인하면서 api 호출은 한번만
               Deps _ 함수내에서 원하는 것만 호출하기 (ex)
                      클릭카운트 , 키입력값 출력 두개동시가아닌 원하는것만

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import Button from "./Button"
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
+function Hello() {
+  useEffect(function () {
+    console.log("hi :)");
+    return function () {
+      console.log("bye :(");
+    };
+  }, []);
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  }, []);
+  return <h1>Hello</h1>;
+}
 
 function App() {
+  const [showing, setShowing] = useState(false);
   const [counter, setValue] = useState(0);
   const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
+  const onClick = () => setShowing((prev) => !prev);
   const onChange = (event) => setKeyword(event.target.value);
   useEffect(() => {
     console.log("I run only once.");
@@ -24,6 +38,8 @@ function App() {
       <input value={keyword} onChange={onChange} type="text" placeholder="Search here...." />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
