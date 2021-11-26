8407825596893d95d5cd00d418d13a8ce9816783
weinsub <dnlstjq@naver.com>
2021-11-26 09:48:50 +0900

    2021.11.25 -------------------- 프로젝트 하나 끝. 2021.11.25 cleanup -            Todo List _ map() 사용하기, 간단한 todoList            coinPriceTag _ API 호출해서 useEffect 사용하여,                           처음, 계속 불러오는것 설정하고 불러오기            Detail.js -> 수정중임, 홈에서 status 사용한것을 사용해서                         Json을 이용해서 완성하기 숙제중임.            Detail.js -> 수정완료 안되면 전단계 코드와 비교해서 수정 2021.11.24 Make a react_app            make a button            useEffect() _ 호출이 계속되야하는것과 한번만 호출해도되는것                        즉, 클릭값은 계속확인하면서 api 호출은 한번만            Deps _ 함수내에서 원하는 것만 호출하기 (ex)                   클릭카운트 , 키입력값 출력 두개동시가아닌 원하는것만

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,16 +1,37 @@
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
-function Detail() {
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import "./Detail.css";
+const Detail = () => {
+  const [movie, setMovie] = useState("");
+  const [loading, setLoading] = useState(true);
   const { id } = useParams();
-  const getMovie = async () => {
-    const json = await (
-      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+  const getDetail = async () => {
+    const detail = await (
+        await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    setMovie(detail.data.movie);
+    console.log(movie);
+    setLoading(false);
   };
   useEffect(() => {
-    getMovie();
+    getDetail();
   }, []);
-  return <h1>Detail</h1>;
-}
+  var sectionStyle = {
+    height: "100vh",
+    backgroundPosition: "50%",
+    backgroundImage: "url(" + movie.background_image_original + ")",
+    backgroundSize: "cover",
+  };
+  return (
+      <div>
+        {loading ? (
+            "Loading...."
+        ) : (
+            <div style={sectionStyle}>
+              <img src={movie.medium_cover_image} alt={movie.id}></img>
+            </div>
+        )}
+      </div>
+  );
+};
 export default Detail;
